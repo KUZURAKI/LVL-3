06a90bec69d7040a37a612ec67ba8fb1ebede332
Артём <109219081+KUZURAKI@users.noreply.github.com>
2025-07-10 13:19:00 +0300

    Add files via upload

---
+++ front/static/js/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButtons = document.querySelectorAll(".togglePassword");
+
+  toggleButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const input = this.previousElementSibling;
+
+      if (input.type === "password") {
+        input.type = "text";
+      } else {
+        input.type = "password";
+      }
+    });
+  });
+});
